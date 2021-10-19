7e70db84a4b92a5c191db2f70990bcd1e7704797
Noel <noelcerulean@aol.com>
2021-10-19 00:44:29 -0400

    and shinx

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (80359cb9)";
+Config.version = "0.11.2 (773d917b)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
