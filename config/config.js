82ef3fe8af1703f967703303910ad5373b016ca1
Noel <noelcerulean@aol.com>
2021-12-04 00:05:56 -0500

    luvdisc

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (fd23a43f)";
+Config.version = "0.11.2 (220e730c)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
