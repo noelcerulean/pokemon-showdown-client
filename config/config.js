ff3952634e0a2e603f8e2314dc5d6e1424fcb6a1
Noel <noelcerulean@aol.com>
2022-01-05 22:18:19 -0500

    ability text update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (b91e1e72)";
+Config.version = "0.11.2 (90c1adbe)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
