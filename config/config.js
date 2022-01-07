1ee9fb7c25aa67a3dd50f3fb4995faaf17bad794
Noel <noelcerulean@aol.com>
2022-01-06 20:30:17 -0500

    delibird learning naughty or nice

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (07ccead5)";
+Config.version = "0.11.2 (f23eb5c2)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
