53ccc7941bf42e4dc1931f2fb1065e9727b85848
Noel <noelcerulean@aol.com>
2022-07-22 13:50:19 -0700

    persian, shiinotic, eelektross, amulet coin, pay day

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (1b80f166)";
+Config.version = "0.11.2 (adb08c21)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
