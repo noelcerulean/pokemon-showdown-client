1d687d2cf42178209924f074e9b74640e9f77bb3
Noel <noelcerulean@aol.com>
2023-02-09 17:27:05 -0700

    sparkly spaghetti

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (8d0bae0e)";
+Config.version = "0.11.2 (894047bd)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
