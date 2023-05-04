2b648df0a953e64f277ba4bf322db782a818273e
Noel <noelcerulean@aol.com>
2023-05-04 13:30:19 -0700

    probo test

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (59919d52)";
+Config.version = "0.11.2 (5fa6509a)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
