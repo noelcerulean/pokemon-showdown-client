22794b44d6d994f9d868111cd7425412475ec85d
Noel <noelcerulean@aol.com>
2024-01-05 02:28:51 -0700

    shed ban

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (e72a89a6)";
+Config.version = "0.11.2 (6233195b)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
