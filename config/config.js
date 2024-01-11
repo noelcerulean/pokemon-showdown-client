1d6c81838255130322384436a60ad4f8ba7e564d
Noel <noelcerulean@aol.com>
2024-01-11 00:16:28 -0700

    goro ice punch

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (6913f595)";
+Config.version = "0.11.2 (92cb8d48)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
