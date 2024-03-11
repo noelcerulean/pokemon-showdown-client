399cc0b8d0496059712920d834019ffded16f833
Noel <noelcerulean@aol.com>
2024-03-11 00:54:26 -0700

    nuz-a

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (298135da)";
+Config.version = "0.11.2 (f8f07a4b)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
