f6dd276da41f73d2105ae7a102b0ee14f65410aa
Noel <87045398+noelcerulean@users.noreply.github.com>
2024-07-01 22:05:31 -0700

    Backports Primeape, Alakazam, Dustox, Beautifly, Electrode

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (d2e6adb0)";
+Config.version = "0.11.2 (5b416d64)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
