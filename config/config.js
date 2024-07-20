c241055fe5a96740310765ddc51df6b4d2de058b
Noel <87045398+noelcerulean@users.noreply.github.com>
2024-07-20 14:43:49 -0700

    slight update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (4af4b8d8)";
+Config.version = "0.11.2 (06e91f04)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
