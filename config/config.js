46d53821f28c368279ea033c237df5f64c94e9f8
Noel <87045398+noelcerulean@users.noreply.github.com>
2024-07-22 00:52:31 -0700

    RU Battle Factory

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (06e91f04)";
+Config.version = "0.11.2 (c241055f)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
