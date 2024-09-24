cbd032b7ab673b67db262e098bc0120013e160a1
Noel <87045398+noelcerulean@users.noreply.github.com>
2024-09-23 18:50:33 -0700

    eevee p1

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (75fee9aa)";
+Config.version = "0.11.2 (8fed6078)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
