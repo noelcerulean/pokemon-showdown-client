5713391fed010469b92ff12dc1432426ce6ebbe0
Noel <87045398+noelcerulean@users.noreply.github.com>
2025-04-14 00:32:22 -0700

    nu bans next

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (68d986a4)";
+Config.version = "0.11.2 (05cb07ad)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
