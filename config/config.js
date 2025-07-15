10244b3fab83e0a54457dbedd505390bc4e14721
Noel <87045398+noelcerulean@users.noreply.github.com>
2025-07-15 00:03:57 -0700

    new skins dark stuff distrib

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (d2f9c116)";
+Config.version = "0.11.2 (d74d8cd2)";
 
 Config.routes = {
 	root: 'fnf-showdown.herokuapp.com',
